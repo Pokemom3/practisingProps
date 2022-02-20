ad50855c588844cfc8b1cd8280529b64b0f8f0a4
unknown <tanmatthews1@gmail.com>
2022-02-20 13:44:33 +0000

    updated age with a useState as an integer instead of a string

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Button = () => {
-  const handleClick = (name, e) => {
-    console.log("hello " + name, e.target);
+  const [age, setAge] = useState(32);
+  const handleClick = () => {
+    setAge(47);
   };
 
   return (
     <div>
+      <p>{age}</p>
       <button
-        onClick={
-          (e) => handleClick("Lela", e)
+        onClick={handleClick}
 
-          /*handleClick ()
+        /*handleClick ()
        if you want to call the function straight away without pressing the button use the parenthesis,
         {handleClick()}otherwise take the parentheses off (do not call the function*/
-        }
       >
         Click here
       </button>
